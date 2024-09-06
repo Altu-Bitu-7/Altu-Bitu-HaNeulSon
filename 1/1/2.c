2ca3fe9f6bed1eaef126d8bc41c229e67be6d178
methal <handsson@ewhain.net>
2024-09-06 20:40:41 +0900

    hi

---
+++ 1/1/2.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define N 10
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+	int freq[6] = { 0 };
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 10000; i++)
+		++freq[rand() % 6];
+	printf("숫자        횟수\n");
+	
+	for (int i = 0; i < 6; i++)
+		printf("%d        %d\n", i + 1, freq[i]);
+}

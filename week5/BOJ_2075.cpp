a0653dc75ec5d7ad96f9c40f41b868c77212ee0d
methal <handsson@ewhain.net>
2024-09-17 17:54:00 +0900

    0917 commit

---
+++ week5/BOJ_2075.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int num;
+            cin >> num;
+            
+            // 힙에 N개가 되기 전까지는 삽입
+            if (minHeap.size() < N) {
+                minHeap.push(num);
+            } else {
+                // N개 이상; 새로운 수가 힙의 최소값보다 크면 교체
+                if (num > minHeap.top()) {
+                    minHeap.pop();
+                    minHeap.push(num);
+                }
+            }
+        }
+    }
+    
+    // 이미 더 작은 N-1개를 pop()했으니 top()이 N번째 수
+    cout << minHeap.top() << endl;
+    return 0;
+}
---
+++ week5/BOJ_2607.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+string S;
+vector<int> firstWordCnt;
+
+vector<int> countFreq(string s) {
+    vector<int> ret(26, 0);
+    
+    for (auto c: s) {
+        ret[c-'A']++;
+    }
+    
+    return ret;
+}
+
+bool check(string word) {
+    vector<int> cnt = countFreq(word);
+    vector<int> diff;
+    
+    for (int i=0; i<26; i++) {
+        if (cnt[i] != firstWordCnt[i]) {
+            diff.push_back(cnt[i] - firstWordCnt[i]);
+        }
+    }
+    
+    if (diff.empty()) return true;
+
+    if (diff.size() == 1 && abs(diff[0]) == 1) return true;
+
+    if (diff.size() == 2 && abs(diff[0]) == true && diff[0] + diff[1] == false) return true;
+    
+    return false;
+}
+
+int solution() {
+    int ret = 0;
+    
+    firstWordCnt = countFreq(S);
+    
+    for (int i=0; i<N-1; i++) {
+        string temp;
+        cin >> temp;
+        
+        if (check(temp)) ret++;
+    }
+    
+    return ret;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> N;
+    cin >> S;
+
+    cout << solution();
+    
+    return 0;
+}
